760552ac2bd6e97ccead25ff2b12367ab3713431
Ross Katz <ross.katz@corrdyn.com>
2023-11-30 03:14:06 +0000

    fix beta encoding

--- dbt/models/all_continuous/bronze/terpenes_serum_surplus_laboratory.sql
+++ dbt/models/all_continuous/bronze/terpenes_serum_surplus_laboratory.sql
@@ -9,26 +9,26 @@ ELSE SAFE_CAST(WTSSTP2Y AS FLOAT64)
 CASE
     WHEN SSSAPN IS NULL THEN NULL 
 ELSE SAFE_CAST(SSSAPN AS FLOAT64) 
- END as î_pinene_ng_ml, 
+ END as i_pinene_ng_ml, 
 
 CASE
     WHEN SAFE_CAST(ROUND(SAFE_CAST(SSSAPNL AS FLOAT64),0) AS INT64) = SAFE_CAST(ROUND(SAFE_CAST(0 AS FLOAT64),0) AS INT64) THEN 'At or above the detection limit' -- categorize numeric values
 WHEN SAFE_CAST(ROUND(SAFE_CAST(SSSAPNL AS FLOAT64),0) AS INT64) = SAFE_CAST(ROUND(SAFE_CAST(1 AS FLOAT64),0) AS INT64) THEN 'Below lower detection limit' -- categorize numeric values
 WHEN SSSAPNL IS NULL THEN NULL 
 ELSE SAFE_CAST(SSSAPNL AS STRING) 
- END as î_pinene_comment_code, 
+ END as i_pinene_comment_code, 
 
 CASE
     WHEN SSSBPN IS NULL THEN NULL 
 ELSE SAFE_CAST(SSSBPN AS FLOAT64) 
- END as î²_pinene_ng_ml, 
+ END as beta_pinene_ng_ml, 
 
 CASE
     WHEN SAFE_CAST(ROUND(SAFE_CAST(SSSBPNL AS FLOAT64),0) AS INT64) = SAFE_CAST(ROUND(SAFE_CAST(0 AS FLOAT64),0) AS INT64) THEN 'At or above the detection limit' -- categorize numeric values
 WHEN SAFE_CAST(ROUND(SAFE_CAST(SSSBPNL AS FLOAT64),0) AS INT64) = SAFE_CAST(ROUND(SAFE_CAST(1 AS FLOAT64),0) AS INT64) THEN 'Below lower detection limit' -- categorize numeric values
 WHEN SSSBPNL IS NULL THEN NULL 
 ELSE SAFE_CAST(SSSBPNL AS STRING) 
- END as î²_pinene_comment_code, 
+ END as beta_pinene_comment_code, 
 
 CASE
     WHEN SSSLIM IS NULL THEN NULL 
