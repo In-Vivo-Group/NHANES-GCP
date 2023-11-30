452485f86a975a5475754e6d246d9c36bcd23c98
Ross Katz <ross.katz@corrdyn.com>
2023-11-30 20:31:50 +0000

    add new models

---
+++ dbt/analyses/ml_coefficients/food_security_to_instrumental_activity_limitation_logistic_regression_outputs.sql
@@ -0,0 +1,13 @@
+SELECT
+  *,
+  ROUND(weight,4) as coefficient,
+  CASE
+  WHEN p_value <= .0001 THEN '*** < .0001'
+  WHEN p_value <= .001 then '** <.001' 
+  WHEN p_value <= .01 then '* <.01' 
+  WHEN p_value <= .05 then '. <.05'
+  ELSE 'not significant'
+  END as p_value_category
+FROM
+  ML.ADVANCED_WEIGHTS(MODEL `nhanes-genai.nhanes.instrumental_activity_limitation_food_security_logistic_regression`, STRUCT(true AS standardize))
+  ORDER BY category DESC;
