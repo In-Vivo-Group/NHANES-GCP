0915658a928cda721f356f6a77d9d88832b816d1
abdulktx1 <37246694+abdulktx1@users.noreply.github.com>
2023-12-13 11:42:50 -0600

    update processing script for compute

--- infra/startup-script.sh
+++ infra/startup-script.sh
@@ -25,7 +25,48 @@ source myenv/bin/activate
 python --version > /tmp/python-version.txt
 
 ## Data processing steps TODO: here
+git clone git@github.com:In-Vivo-Group/NHANES-GPT.git
+cd NHANES-GPT
+poetry install
+
+# TODO - Set evironment variables
+# export PROJECT_ID=...
+# export BUCKET_NAME=...
+# export GCP_KEY_ID=$(gcloud secrets versions access latest --secret=GCP_KEY_ID)
+# export GCP_KEY=$(gcloud secrets versions access latest --secret=GCP_KEY)
+# execute the data processing steps
+cd NHANES-GPT/ETL
+python scrape_nhanes_metadata.py
+sleep 30
+
+python scrape_nhanes_data_files.py
+
+sleep 30
+# Expexted to take 3 hours
+echo "Starting data scraping - expected to take 3 hours"
+python scrape_nhanes_data_files.py
+
+sleep 30
+
+# Expexted to take 40 mins
+echo "Starting data processing"
+python create_nhanes_dataset.py
 
 
+# Store column level data
 
+bash get_bq_schemas.sh
 
+# Create data models
+
+python generate_dbt_models.py
+
+# Finally, run dbt
+
+dbt build --select all_continuous.staging.* all_continuous.bronze.*
+
+# Sleep for 5 mins to allow for dbt to finish
+sleep 300
+
+# Shutdown the instance
+sudo shutdown -h now
